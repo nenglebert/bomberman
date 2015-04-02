07101d4dd4fae090b110a2df179c399bd390681a
nenglebert <n.englebert@gmail.com>
2015-04-02 21:24:19 +0200

    Ajout classe main : Bomberman

---
+++ Bomberman/src/Bomberman.java
@@ -0,0 +1,12 @@
+
+public class Bomberman {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Bomberman/src/LaClasse.java
+++
@@ -1,4 +0,0 @@
-
-public class LaClasse {
-//Je sais
-}
