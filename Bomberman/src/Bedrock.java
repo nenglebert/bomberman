30f264402d165037ae2673b00b1de5e516687a0f
Enes-Ulusoy <Enes-Ulusoy@172.34.36.170>
2015-04-20 11:48:36 +0200

    changement de PExplose en NExplose

--- Bomberman/src/Bedrock.java
+++ Bomberman/src/Bedrock.java
@@ -3,7 +3,7 @@ public class Bedrock extends Element{
 	
 	public Bedrock(int posx, int posy){
 		//this.skin = ;
-		this.explose = new PExplose();
+		this.explose = new NExplose();
 		this.posx = posx;
 		this.posy = posy;
 		this.life = 1;
