207ed4e5ebeb4445732d6a7ebaa7ee399544dcae
Enes-Ulusoy <Enes-Ulusoy@172.34.36.170>
2015-04-20 11:47:01 +0200

    Création des classes

---
+++ Bomberman/src/Bedrock.java
@@ -0,0 +1,11 @@
+
+public class Bedrock extends Element{
+	
+	public Bedrock(int posx, int posy){
+		//this.skin = ;
+		this.explose = new PExplose();
+		this.posx = posx;
+		this.posy = posy;
+		this.life = 1;
+	}
+}
---
+++ Bomberman/src/Block.java
@@ -0,0 +1,11 @@
+
+public class Block extends Element{
+	
+	public Block(int posx, int posy){
+		//this.skin = ;
+		this.explose = new PExplose();
+		this.posx = posx;
+		this.posy = posy;
+		this.life = 1;
+	}
+}
