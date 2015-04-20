95b1b8283030e44bc7e865849fbc203f1bf72e0b
Enes-Ulusoy <Enes-Ulusoy@172.34.36.170>
2015-04-20 11:42:38 +0200

    Création de la classe bombe

---
+++ Bomberman/src/Bomb.java
@@ -0,0 +1,11 @@
+
+public class Bomb extends Element {
+	
+	public Bomb(int posx, int posy){
+		//this.skin = ;
+		this.explose = new PExplose();
+		this.posx = posx;
+		this.posy = posy;
+		this.life = 1;
+	}
+}
