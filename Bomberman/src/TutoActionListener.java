c9a02088ce3d58b0d57219b5a5a9f5fe06fc13ba
f380cedric <f380cedric@users.noreply.github.com>
2015-05-07 18:33:45 +0200

    resolu

--- Bomberman/src/TutoActionListener.java
+++ Bomberman/src/TutoActionListener.java
@@ -2,8 +2,6 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
-
 
 public class TutoActionListener implements ActionListener {
 	GameWindow gameWindow;
